154569b25690c551b4e4fdcbae76beddb9044424
samartin <samartin@student.42madrid.com>
2023-01-18 12:27:42 +0100

    2023 1 18

--- is_pow2.c
+++ is_pow2.c
@@ -6,24 +6,24 @@
 /*   By: samartin <samartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 11:28:52 by samartin          #+#    #+#             */
-/*   Updated: 2023/01/18 12:03:41 by samartin         ###   ########.fr       */
+/*   Updated: 2023/01/18 12:23:46 by samartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
  * Checks if a number is power of 2. That is true if there is a single positive
- * bit in the number and it is not 1. To check it, we move the bits to the right
- * until there is any in position 2 (n & 2 == true), if at that point n == 2,
- * its original value was power of 2, otherwise it was not, because there were
- * some other positive bits.
+ * bit in the number. To check it, we move the bits to the right until there is
+ * any in position 1 (n & 1 == true), if at that point n == 1, its original
+ * value was power of 2, otherwise it was not, because there were some other
+ * positive bits. (1 is also power of 2: 1 = 2^0).
  */
 int	is_pow2(unsigned int n)
 {
-	while (n >= 2)
+	while (n >= 1)
 	{
-		if (n & 2)
+		if (n & 1)
 		{
-			if (n == 2)
+			if (n == 1)
 				return (1);
 			else
 				return (0);
